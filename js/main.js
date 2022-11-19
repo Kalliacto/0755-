81d2fcb0f8ab622c551ec838259c9a071999367f
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2022-11-19 15:55:40 +0300

    Добавлена кнопка вверх

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-let page = document.querySelector('.page')
+const theBlog = document.querySelector('.the_blog');
+const page = document.querySelector('.page');
+const themeButton = document.querySelector('.theme-button');
+
+
+
+theBlog.addEventListener('click', () => {
+    alert("Данный раздел пока не добавлен, находится в разработке!");
+});
+
+// ------------------------------------------
 if (localStorage.id_theme == null) {
     localStorage.id_theme = 0;
 }
@@ -7,8 +17,9 @@ if (localStorage.id_theme == 0) {
 }
 else {
     page.classList.add('dark-theme');
-}
-let themeButton = document.querySelector('.theme-button');
+};
+// ------------------------------------------
+
 themeButton.onclick = function () {
     if (localStorage.id_theme == 0) {
         localStorage.id_theme = 1;
@@ -18,7 +29,10 @@ themeButton.onclick = function () {
         localStorage.id_theme = 0;
         page.classList.remove('dark-theme');
     }
-}
+};
+// ------------------------------------------
+
+
 
 // let page = document.querySelector('.page')
 // if (sessionStorage.id_theme == null) {
@@ -57,6 +71,62 @@ $('.paulund_modal a').on('click', function () {
 $('.close_popup').on('click', function () {
     $('.popups__inner').removeClass('active');
 });
+/*------------------------------------------*/
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    scrolling: false,
+    show() {
+        if (this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+            this.el.classList.remove('btn-up_hide');
+            this.el.classList.add('btn-up_hiding');
+            window.setTimeout(() => {
+                this.el.classList.remove('btn-up_hiding');
+            }, 300);
+        }
+    },
+    hide() {
+        if (!this.el.classList.contains('btn-up_hide') && !this.el.classList.contains('btn-up_hiding')) {
+            this.el.classList.add('btn-up_hiding');
+            window.setTimeout(() => {
+                this.el.classList.add('btn-up_hide');
+                this.el.classList.remove('btn-up_hiding');
+            }, 300);
+        }
+    },
+    addEventListener() {
+        // при прокрутке окна (window)
+        window.addEventListener('scroll', () => {
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            if (this.scrolling && scrollY > 0) {
+                return;
+            }
+            this.scrolling = false;
+            // если пользователь прокрутил страницу более чем на 200px
+            if (scrollY > 400) {
+                // сделаем кнопку .btn-up видимой
+                this.show();
+            } else {
+                // иначе скроем кнопку .btn-up
+                this.hide();
+            }
+        });
+        // при нажатии на кнопку .btn-up
+        document.querySelector('.btn-up').onclick = () => {
+            this.scrolling = true;
+            this.hide();
+            // переместиться в верхнюю часть страницы
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+    }
+}
+
+btnUp.addEventListener();
+
+
 
 
 
